cec9ed427eb2dff2a89a1f4132c961f356c5f436
Carolina Zampieri Rodrigues <carolina.zampieri89@gmail.com>
2022-08-27 19:54:36 -0300

    Resolução

---
+++ script.js
@@ -0,0 +1,8 @@
+//convert celsius to fahrenheit
+
+function convertToFahrenheit(value) {
+    return value * 1.8 + 32
+}
+
+let result = convertToFahrenheit(20)
+console.log(`O valor em Fahrenheit é ${result}`)
